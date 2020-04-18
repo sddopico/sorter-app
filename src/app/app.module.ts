71f992e5b64d3d49c52b1f47487090c568c1bde0
Dominic Dopico <serafin.dopico@gmail.com>
2020-04-18 16:00:46 -0700

    consolidate shared components in shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,16 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatButtonModule } from "@angular/material/button";
-import { ShellComponent } from "./shared/shell/shell.component";
-import { LayoutModule } from "@angular/cdk/layout";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatIconModule } from "@angular/material/icon";
-import { MatListModule } from "@angular/material/list";
+
 import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
-  declarations: [AppComponent, ShellComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
+
     SharedModule
   ],
   providers: [],
